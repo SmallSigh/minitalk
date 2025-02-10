f3631e2f3c94c69d6fbead94d8b81124d2e71cff
SmallSigh <martijnsmit4@gmail.com>
2025-02-10 20:02:50 +0100

    scrapping everything...

--- client/client.c
+++ client/client.c
@@ -1,3 +1,80 @@
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   client.c                                           :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: masmit <masmit@student.42.fr>              +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2025/02/07 19:49:45 by masmit            #+#    #+#             */
+// /*   Updated: 2025/02/10 19:57:13 by masmit           ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
+
+// #include "ft_printf.h"
+// #include "libft.h"
+// #include "minitalk.h"
+
+// void	send_bit(pid_t server_pid, short bit)
+// {
+// 	if (bit == 1)
+// 		kill(server_pid, SIGUSR1);
+// 	else
+// 		kill(server_pid, SIGUSR2);
+// 	usleep(100);
+// }
+
+// void	send_message(pid_t server_pid, char *message)
+// {
+// 	int	i;
+// 	int	bit;
+
+// 	i = 0;
+// 	while (message[i])
+// 	{
+// 		bit = 7;
+// 		while (bit >= 0)
+// 		{
+// 			send_bit(server_pid, (message[i] >> bit) & 1);
+// 			bit--;
+// 		}
+// 		i++;
+// 	}
+// 	ft_printf("Message has been sent...\n");
+// }
+
+// // least significant bit first;
+// void	send_length(pid_t server_pid, uint32_t message_length)
+// {
+// 	int	bit;
+
+// 	bit = 31;
+// 	while (bit >= 0)
+// 	{
+// 		send_bit(server_pid, (message_length >> bit) & 1);
+// 		bit--;
+// 	}
+// 	ft_printf("Message length has been sent...\n");
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	pid_t		server_pid;
+// 	uint32_t	message_length;
+	
+// 	if (argc != 3)
+// 	{
+// 		ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
+// 		return (1);
+// 	}
+// 	server_pid = ft_atoi(argv[1]);
+// 	message_length = 0;
+// 	while (argv[2][message_length])
+// 		message_length++;
+// 	send_length(server_pid, message_length);
+// 	send_message(server_pid, argv[2]);
+// 	ft_printf("Signal sent to PID <%d>\n", server_pid);
+// 	return (0);
+// }
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -23,15 +100,17 @@ void	send_bit(int server_pid, short bit)
 	usleep(100);
 }
 
+// Sending message in **BIG-ENDIAN** (Most Significant Bit First)
 void	send_message(int server_pid, char *message)
 {
 	int	i;
 	int	bit;
 
+	i = 0;
 	while (message[i])
 	{
 		bit = 7;
-		while (bit >= 0)
+		while (bit >= 0) // Send from MSB (bit 7) to LSB (bit 0)
 		{
 			send_bit(server_pid, (message[i] >> bit) & 1);
 			bit--;
@@ -41,13 +120,13 @@ void	send_message(int server_pid, char *message)
 	ft_printf("Message has been sent...\n");
 }
 
-// least significant bit first;
+// Sending message length in **BIG-ENDIAN** (Most Significant Bit First)
 void	send_length(int server_pid, uint32_t message_length)
 {
 	int	bit;
 
 	bit = 31;
-	while (bit >= 0)
+	while (bit >= 0) // Send from MSB (bit 31) to LSB (bit 0)
 	{
 		send_bit(server_pid, (message_length >> bit) & 1);
 		bit--;
@@ -62,15 +141,17 @@ int	main(int argc, char **argv)
 	
 	if (argc != 3)
 	{
-		ft_printf("Usage: %s <server_pid> <1 or 2>\n", argv[0]);
+		ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
 		return (1);
 	}
 	server_pid = ft_atoi(argv[1]);
 	message_length = 0;
 	while (argv[2][message_length])
 		message_length++;
+	
 	send_length(server_pid, message_length);
 	send_message(server_pid, argv[2]);
+
 	ft_printf("Signal sent to PID <%d>\n", server_pid);
 	return (0);
 }
--- server/server.c
+++ server/server.c
@@ -1,3 +1,95 @@
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   server.c                                           :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: masmit <masmit@student.42.fr>              +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2025/02/07 20:27:00 by masmit            #+#    #+#             */
+// /*   Updated: 2025/02/10 19:58:15 by masmit           ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
+
+// #include "ft_printf.h"
+// #include "libft.h"
+// #include "minitalk.h"
+
+// // m_d = message data;
+// void handle_message_data(t_message_info *m_d, int received_bit)
+// {
+// 	m_d->current_char |= (received_bit << (7 - m_d->bit));
+// 	m_d->bit++;
+// 	if (m_d->bit == 8)
+// 	{
+// 		m_d->message[m_d->char_index] = m_d->current_char;
+// 		ft_printf("%c", m_d->current_char);
+// 		m_d->char_index++;
+// 		m_d->bit = 0;
+// 		m_d->current_char = 0;
+// 		if (m_d->char_index == m_d->message_length)
+// 		{
+// 			ft_printf("Message has been received: %s\n", m_d->message);
+// 			free(m_d->message);
+// 			m_d->message = NULL;
+// 			m_d->message_length = 0;
+// 			m_d->char_index = 0;
+// 			m_d->bit = 0;
+// 			m_d->current_char = 0;
+// 			m_d->receiving_length = 1;
+// 		}
+// 	}
+// }
+
+// void handle_message_length(t_message_info *message_data, int received_bit)
+// {
+// 	message_data->message_length |= (received_bit << (31 - message_data->bit));
+// 	message_data->bit++;
+// 	if (message_data->bit == 32)
+// 	{
+// 		message_data->message = malloc(message_data->message_length + 1);
+// 		if (!message_data->message)
+// 		{
+// 			ft_printf("Something went wrong receiving the message, please try again.\n");
+// 			exit(1);
+// 		}
+// 		ft_printf("Message length received! Making room for a message of size %d.\n", message_data->message_length);
+// 		message_data->message[message_data->message_length] = '\0';
+// 		message_data->receiving_length = 0;
+// 		message_data->bit = 0;
+// 	}
+// }
+
+// void signal_handler(int signum, siginfo_t *info, void *context)
+// {
+// 	static	t_message_info message_data = {NULL, 0, 0, 0, 1, 0};
+// 	int		received_bit;
+
+// 	(void)info;
+// 	(void)context;
+// 	received_bit = (signum == SIGUSR1) ? 1 : 0;
+// 	if (message_data.receiving_length)
+// 		handle_message_length(&message_data, received_bit);
+// 	else
+// 		handle_message_data(&message_data, received_bit);
+// }
+
+// int main(void)
+// {
+// 	struct sigaction	sa;
+// 	pid_t				pid;
+
+// 	pid = getpid();
+// 	ft_printf("Server PID = <%d>\n", pid);
+// 	sa.sa_flags = SA_SIGINFO;
+// 	sa.sa_sigaction = signal_handler;
+// 	sigemptyset(&sa.sa_mask);
+// 	sigaction(SIGUSR1, &sa, NULL);
+// 	sigaction(SIGUSR2, &sa, NULL);
+
+// 	while (1)
+// 		pause();
+// }
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,7 +98,7 @@
 /*   By: masmit <masmit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 20:27:00 by masmit            #+#    #+#             */
-/*   Updated: 2025/02/10 18:45:20 by masmit           ###   ########.fr       */
+/*   Updated: 2025/02/10 18:49:32 by masmit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +113,7 @@ void handle_message_data(t_message_info *m_d, int received_bit)
 	m_d->bit++;
 	if (m_d->bit == 8)
 	{
-		m_d->message[m_d->char_index++] = m_d->current_char;
+		m_d->message[m_d->char_index] = m_d->current_char;
 		ft_printf("%c", m_d->current_char);
 		m_d->char_index++;
 		m_d->bit = 0;
@@ -61,7 +153,7 @@ void handle_message_length(t_message_info *message_data, int received_bit)
 
 void signal_handler(int signum, siginfo_t *info, void *context)
 {
-	static	t_message_info message_data = {NULL, 0, 0, 0, 1, 0};;
+	static	t_message_info message_data = {NULL, 0, 0, 0, 1, 0};
 	int		received_bit;
 
 	(void)info;
@@ -76,7 +168,7 @@ void signal_handler(int signum, siginfo_t *info, void *context)
 int main(void)
 {
 	struct sigaction	sa;
-	__pid_t				pid;
+	pid_t				pid;
 
 	pid = getpid();
 	ft_printf("Server PID = <%d>\n", pid);
